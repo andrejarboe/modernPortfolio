a45f6e23ee293787aa32a3d734f75fb0b8057b8a
andrejarboe <andrejarboe@gmail.com>
2022-09-30 17:49:18 -0400

    installed tailwind

---
+++ client/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Andre's Portfolio | Home</title>
+
+      </Head>
+      <h1 className='text-3xl font-bold underline text-red-500'>Hey! Lets build an awesome Portfolio</h1>
+    </div>
+  )
+}
+
+export default Home
--- readme.md
+++ readme.md
@@ -1,1 +1,14 @@
 # Modern Portfolio
+
+## My steps:
+- npx create-next-app@latest --ts client
+- cd client
+- npm install -D tailwindcss postcss autoprefixer
+- npx tailwindcss init -p
+- add to tailwind config:
+    ```
+     content: [
+        './pages/**/*.{js,ts,jsx.tsx}',
+        './components/**/*.{js,ts,jsx.tsx}'
+    ],
+    ```
