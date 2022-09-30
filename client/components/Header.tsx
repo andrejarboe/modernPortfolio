c634531c6d22756c70bf97a081100a1f89e14528
andrejarboe <andrejarboe@gmail.com>
2022-09-30 18:38:10 -0400

    add email animation

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -7,21 +7,21 @@ type Props = {}
 function Header({ }: Props) {
     return (
         <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
-            <motion.div 
-            initial={{
-                x:-500,
-                opacity: 0,
-                scale: 0.5,
-            }}
-            animate={{
-                x:0,
-                opacity: 1,
-                scale: 1
-            }}
-            transition={{
-                duration: 1.5
-            }}
-            className='flex flex-row items-center'>
+            <motion.div
+                initial={{
+                    x: -500,
+                    opacity: 0,
+                    scale: 0.5,
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1,
+                    scale: 1
+                }}
+                transition={{
+                    duration: 1.5
+                }}
+                className='flex flex-row items-center'>
                 {/* Social Icons  */}
                 <SocialIcon
                     url="https://www.linkedin.com/in/andre-jarboe-ii/"
@@ -39,7 +39,21 @@ function Header({ }: Props) {
                     bgColor='transparent'
                 />
             </motion.div>
-            <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+            <motion.div
+                initial={{
+                    x: 500,
+                    opacity: 0,
+                    scale: 0.5,
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1,
+                    scale: 1
+                }}
+                transition={{
+                    duration: 1.5
+                }}
+                className='flex flex-row items-center text-gray-300 cursor-pointer'>
                 <SocialIcon
                     className='cursor-pointer'
                     network="email"
@@ -47,7 +61,7 @@ function Header({ }: Props) {
                     bgColor='transparent'
                 />
                 <p className='uppercase hidden md:inline-flex text-small text-gray-400'>Get In Touch</p>
-            </div>
+            </motion.div>
         </header>
     )
 }
