6c579942e9913226881aa1beffd5387187e90fc3
andrejarboe <andrejarboe@gmail.com>
2022-10-02 19:33:56 -0400

    mvp

--- client/components/Projects2.tsx
+++ client/components/Projects2.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { SocialIcon } from 'react-social-icons';
+import { urlFor } from '../sanity';
 import { Project } from '../typings';
 
 type Props = {
@@ -7,116 +8,6 @@ type Props = {
 }
 
 export default function Projects2({ projects }: Props) {
-    const oldProjects = [
-        {
-            title: "Boost your conversion rate",
-            href: "#",
-            category: { name: "Java", href: "#" },
-            description:
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.",
-            date: "Mar 16, 2020",
-            datetime: "2020-03-16",
-            imageUrl:
-                "https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80",
-            readingTime: "6 min",
-            author: {
-                name: "Roel Aufderehar",
-                href: "#",
-                imageUrl:
-                    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-            },
-        },
-        {
-            title: "How to use search engine optimization to drive sales",
-            href: "#",
-            category: { name: "C++", href: "#" },
-            description:
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis asperiores porro quaerat doloribus, eveniet dolore. Adipisci tempora aut inventore optio animi., tempore temporibus quo laudantium.",
-            date: "Mar 10, 2020",
-            datetime: "2020-03-10",
-            imageUrl:
-                "https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80",
-            readingTime: "4 min",
-            author: {
-                name: "Brenna Goyette",
-                href: "#",
-                imageUrl:
-                    "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-            },
-        },
-        {
-            title: "Apple Watch app email",
-            href: "#",
-            category: { name: "HTML Email", href: "#" },
-            description:
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.",
-            date: "Feb 12, 2020",
-            datetime: "2020-02-12",
-            imageUrl:
-                "https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80",
-            readingTime: "11 min",
-            author: {
-                name: "Daniela Metz",
-                href: "#",
-                imageUrl:
-                    "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-            },
-        },
-        {
-            title: "Apple Watch app email",
-            href: "#",
-            category: { name: "HTML Email", href: "#" },
-            description:
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.",
-            date: "Feb 12, 2020",
-            datetime: "2020-02-12",
-            imageUrl:
-                "https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80",
-            readingTime: "11 min",
-            author: {
-                name: "Daniela Metz",
-                href: "#",
-                imageUrl:
-                    "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-            },
-        },
-        {
-            title: "How to use search engine optimization to drive sales",
-            href: "#",
-            category: { name: "C++", href: "#" },
-            description:
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis asperiores porro quaerat doloribus, eveniet dolore. Adipisci tempora aut inventore optio animi., tempore temporibus quo laudantium.",
-            date: "Mar 10, 2020",
-            datetime: "2020-03-10",
-            imageUrl:
-                "https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80",
-            readingTime: "4 min",
-            author: {
-                name: "Brenna Goyette",
-                href: "#",
-                imageUrl:
-                    "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-            },
-        },
-        {
-            title: "How to use search engine optimization to drive sales",
-            href: "#",
-            category: { name: "C++", href: "#" },
-            description:
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis asperiores porro quaerat doloribus, eveniet dolore. Adipisci tempora aut inventore optio animi., tempore temporibus quo laudantium.",
-            date: "Mar 10, 2020",
-            datetime: "2020-03-10",
-            imageUrl:
-                "https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80",
-            readingTime: "4 min",
-            author: {
-                name: "Brenna Goyette",
-                href: "#",
-                imageUrl:
-                    "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-            },
-        },
-    ];
 
     return (
         <div className='flex flex-col max-w-7xl mx-auto px-4'>
@@ -134,7 +25,7 @@ export default function Projects2({ projects }: Props) {
                             <img
                                 className="h-48 w-full object-cover"
                                 // src={project.imageUrl}
-                                src={project.image.asset._ref}
+                                src={urlFor(project?.image).url()}
                                 alt=""
                             />
                         </div>
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import type { GetStaticProps } from 'next'
 import Head from 'next/head'
 import ContactMe from '../components/ContactMe'
