fd7d79c0eb4e685718311e183e433720cfbbe73b
andrejarboe <andrejarboe@gmail.com>
2022-10-02 14:06:09 -0400

    made pageInfo schema

---
+++ sanity/schemas/pageInfo.js
@@ -0,0 +1,44 @@
+export default {
+  name: 'pageInfo',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "role",
+      title: "Role",
+      type: "string",
+    },
+    {
+      name: "heroImage",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "backgroundInformation",
+      title: "BackgroundInformation",
+      type: "string",
+    },
+    {
+      name: "profilePic",
+      title: "ProfilePic",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "socials",
+      title: "Socials",
+      type: "array",
+      of: [{ type: "reference", to: { type: "social" } }],
+    },
+  ]
+}
