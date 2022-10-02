3029dc0e5caf798c8c1da7a86a705b6a3bb52666
andrejarboe <andrejarboe@gmail.com>
2022-10-02 16:57:12 -0400

    getExperience

--- client/pages/api/getExperience.ts
+++ client/pages/api/getExperience.ts
@@ -2,21 +2,24 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { groq } from "next-sanity";
 import { sanityClient } from "../../sanity";
-import { PageInfo } from "../../typings";
+import { Experience } from "../../typings";
 
 const query = groq`
-    *[_type == "pageInfo"][0]
+    *[_type == "experience"]{
+        ...,
+        technologies[]->
+    }
 `;
 
 type Data = {
-    pageInfo: PageInfo;
+    experiences: Experience[];
 };
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>
 ) {
-    const pageInfo: PageInfo = await sanityClient.fetch(query);
+    const experiences: Experience[] = await sanityClient.fetch(query);
 
-    res.status(200).json({ pageInfo });
+    res.status(200).json({ experiences });
 }
